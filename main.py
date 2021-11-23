659373803334947d79cb7606ab8113857756bf0e
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 15:50:17 -0300

    Feat: add mocked /about endpoint

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 def index():
     return "Hello, world!"
+
+
+@app.get("/about")
+def about():
+    return {
+        "data": "about page"
+    }
