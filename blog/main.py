28aac5235df341419df6997f12c18cd682cd0248
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-24 17:28:35 -0300

    Feat: add 404 response if blog is not available for deletion

--- blog/main.py
+++ blog/main.py
@@ -60,6 +60,12 @@ def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_d
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(blog_id: int, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == blog_id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {blog_id} is not available")
+
+    blog.delete(synchronize_session=False)
     db.commit()
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
