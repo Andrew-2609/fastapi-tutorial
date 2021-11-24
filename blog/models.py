8d416198be6aaefdb58970fdb014b093eeca592c
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-24 17:56:50 -0300

    Fix: correct the blogs table name

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from .database import Base
 
 
 class Blog(Base):
-    __tablename__ = "blog"
+    __tablename__ = "blogs"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
