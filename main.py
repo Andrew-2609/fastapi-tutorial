d1fc02671b3c5e0b74149aa1ff0dcc6db1500429
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-24 15:05:49 -0300

    Chore: add main method to debug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -34,3 +35,7 @@ class Blog(BaseModel):
 @app.post("/blog")
 def create_blog(request_body: Blog):
     return {"data": f"Blog was created with title: {request_body.title}"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
