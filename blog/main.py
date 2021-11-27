9c009c01c6db71baaa24bb339e17c96d861d50f5
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-26 21:53:37 -0300

    Feat: add GET endpoint to retrieve Users

--- blog/main.py
+++ blog/main.py
@@ -74,6 +74,16 @@ def delete_blog(blog_id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+@app.get("/user/{id}", response_model=schemas.ShowUser)
+def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} is not available")
+
+    return user
+
+
 @app.post("/user", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     hashed_password = Hash.bcrypt(request.password)
