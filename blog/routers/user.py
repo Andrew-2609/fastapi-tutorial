30a4a1d6a246ef3e98df3b5a66b881b5eeed5cae
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 23:39:34 -0300

    Refactor: move all the CRUD operations of User to repositories/user

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from .. import schemas, models
+from .. import schemas
 from ..database import get_db
-from ..hashing import Hash
+from ..repositories import user
 
 router = APIRouter(
     prefix="/user",
@@ -13,21 +13,9 @@ router = APIRouter(
 
 @router.get("/{id}", response_model=schemas.ShowUser)
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} is not available")
-
-    return user
+    return user.get_by_id(user_id, db)
 
 
 @router.post("/", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    hashed_password = Hash.bcrypt(request.password)
-    new_user = models.User(name=request.name, email=request.email, password=hashed_password)
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
+    return user.create(request, db)
