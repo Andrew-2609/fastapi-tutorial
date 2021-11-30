3972eea85b29612b8021d70fe59ac724aa8f966f
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 22:57:24 -0300

    Refactor: add prefix '/blog' to Blog endpoints

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -7,17 +7,18 @@ from .. import schemas, database, models
 from ..database import get_db
 
 router = APIRouter(
+    prefix="/blog",
     tags=["Blogs"]
 )
 
 
-@router.get("/blog", response_model=List[schemas.ShowBlog])
+@router.get("/", response_model=List[schemas.ShowBlog])
 def get_all_blogs(db: Session = Depends(database.get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@router.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def get_blog_by_id(blog_id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
 
@@ -27,7 +28,7 @@ def get_blog_by_id(blog_id: int, db: Session = Depends(get_db)):
     return blog
 
 
-@router.post("/blog", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
@@ -36,7 +37,7 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
 
@@ -52,7 +53,7 @@ def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_d
     return response
 
 
-@router.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(blog_id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
 
