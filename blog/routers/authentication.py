c08776983a1f07fab795c3e7c26359027a8209c7
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-30 00:01:08 -0300

    Feat: add simple credentials handling in '/login' endpoint of authentication

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from .. import schemas, models
 from ..database import get_db
+from ..hashing import Hash
 
 router = APIRouter(
     tags=["Authentication"]
@@ -14,6 +15,8 @@ def login(request: schemas.Login, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials!")
 
     return user
