2bcbb43e3b90a8383c7b91d73ae6fd79e83b7f5e
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 23:53:08 -0300

    Feat: add basic implementation for an authentication with '/login' endpoint

---
+++ blog/routers/authentication.py
@@ -0,0 +1,19 @@
+from fastapi import Depends, APIRouter, HTTPException, status
+from sqlalchemy.orm import Session
+
+from .. import schemas, models
+from ..database import get_db
+
+router = APIRouter(
+    tags=["Authentication"]
+)
+
+
+@router.post("/login")
+def login(request: schemas.Login, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials!")
+
+    return user
