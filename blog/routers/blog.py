9c801d90039423987d49970e278dee3c8caf7eb3
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-02 10:44:03 -0300

    Feat: add basic authentication for all Blog endpoints

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -21,20 +21,24 @@ def get_all_blogs(db: Session = Depends(database.get_db),
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def get_blog_by_id(blog_id: int, db: Session = Depends(get_db)):
+def get_blog_by_id(blog_id: int, db: Session = Depends(get_db),
+                   current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_by_id(blog_id, db)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db),
+                current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_db),
+                current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(blog_id, request, db)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(blog_id: int, db: Session = Depends(get_db)):
+def delete_blog(blog_id: int, db: Session = Depends(get_db),
+                current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.delete(blog_id, db)
