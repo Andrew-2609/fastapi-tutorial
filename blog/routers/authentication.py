16d66f41284dc20e983265112ff6ee688892d3e1
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-02 10:35:34 -0300

    Feat: set request to be an OAuth2PasswordRequestForm in '/login' endpoint

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import JWTToken
-from .. import schemas, models
+from .. import models
 from ..database import get_db
 from ..hashing import Hash
 
@@ -12,7 +13,7 @@ router = APIRouter(
 
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
 
     if not user:
