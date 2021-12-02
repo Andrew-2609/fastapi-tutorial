59511ac10833eefbbc6d1b4dcf3bb184195a223a
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-02 10:44:51 -0300

    Chore: suppress PyUnusedLocal warning of PyCharm for all Blog routes so far

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -20,24 +20,28 @@ def get_all_blogs(db: Session = Depends(database.get_db),
     return blog.get_all(db)
 
 
+# noinspection PyUnusedLocal
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def get_blog_by_id(blog_id: int, db: Session = Depends(get_db),
                    current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_by_id(blog_id, db)
 
 
+# noinspection PyUnusedLocal
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db),
                 current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
 
 
+# noinspection PyUnusedLocal
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_db),
                 current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(blog_id, request, db)
 
 
+# noinspection PyUnusedLocal
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(blog_id: int, db: Session = Depends(get_db),
                 current_user: schemas.User = Depends(oauth2.get_current_user)):
